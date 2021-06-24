2c37a778da6bef6a29ed74e62e10e4622f8ccb10
tannerrobertsdev <tannerrobertsdev@gmail.com>
2021-06-24 22:07:45 +0900

    complete bookmark,help

--- src/app/folder/bookmark/bookmark.page.ts
+++ src/app/folder/bookmark/bookmark.page.ts
@@ -7,9 +7,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BookmarkPage implements OnInit {
 
+  public tvList = [
+    {
+      title: 'CCTV',
+      data: [{
+        cam: 'PK2 Camera 1',
+        date: '04/06/2021 10:00 AM',
+        img: 'cctvimg1.png'
+      },
+      {
+        cam: 'PK4 Camera 2',
+        date: '04/06/2021 10:00 AM',
+        img: 'cctvimg2.png'
+      }]
+    },
+    {
+      title: 'ECM',
+      data: [{
+        cam: 'PK2 Camera 1',
+        date: '2021/07/07 10:00:56 AM',
+        img: 'ecmimg1.png'
+      }]}
+  ];
+
+  public showList = [false, false];
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  onClickList(index: number) {
+    this.showList[index] = !this.showList[index];
+  }
 }
--- src/app/folder/help/help.page.ts
+++ src/app/folder/help/help.page.ts
@@ -7,9 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HelpPage implements OnInit {
 
+  public helpList = ['How Can We Help?', 'Getting Started', 'Technical Support', 'Contact Us'];
+  public showList = [true, false, false, false];
+  public title = '';
+
   constructor() { }
 
   ngOnInit() {
+    this.title = this.helpList[0];
   }
 
+  onClickList(index: number) {
+    this.title = this.helpList[index];
+    for (let i = 0; i < this.showList.length; i++) {
+      this.showList[i] = false;
+    }
+    this.showList[index] = true;
+  }
 }
